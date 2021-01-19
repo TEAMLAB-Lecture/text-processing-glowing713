3b1bbfac2eecf879ec4683e0c9ffbe74d423d0e5
glowing713 <glowing713@naver.com>
2021-01-19 23:16:11 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -7,5 +7,5 @@ def normalize(input_string):
 
 
 def no_vowels(input_string):
-    no_vowel_string = re.sub('[aeiou]', '', input_string.lower())
+    no_vowel_string = re.sub('[aeiouAEIOU]', '', input_string)
     return no_vowel_string
